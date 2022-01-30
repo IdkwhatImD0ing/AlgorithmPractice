1351837326045930e7ab25dbd5f823646dd4b017
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2022-01-30 13:25:09 -0800

    Small project on predicting profit

---
+++ Python/MachineLearning/ProfitPrediction/LinearRegression.py
@@ -0,0 +1,96 @@
+import os
+import numpy as np
+from matplotlib import pyplot
+from mpl_toolkits.mplot3d import Axes3D
+
+import io
+import pandas as pd
+
+uploaded = open("ex1data1.txt","r")
+data = pd.read_csv(uploaded, header=None).to_numpy()
+X, y = data[:, 0], data[:, 1]
+m = y.size  # number of training examples
+
+def plotData(x, y):
+    """
+    Plots the data points x and y into a new figure. Plots the data 
+    points and gives the figure axes labels of population and profit.
+    
+    Parameters
+    ----------
+    x : array_like
+        Data point values for x-axis.
+
+    y : array_like
+        Data point values for y-axis. Note x and y should have the same size.
+    
+    Instructions
+    ------------
+    Plot the training data into a figure using the "figure" and "plot"
+    functions. Set the axes labels using the "xlabel" and "ylabel" functions.
+    Assume the population and revenue data have been passed in as the x
+    and y arguments of this function.    
+    
+    Hint
+    ----
+    You can use the 'ro' option with plot to have the markers
+    appear as red circles. Furthermore, you can make the markers larger by
+    using plot(..., 'ro', ms=10), where `ms` refers to marker size. You 
+    can also set the marker edge color using the `mec` property.
+    """
+    fig = pyplot.figure()  # open a new figure
+    pyplot.plot(x, y, 'ro', ms=10, mec='k')
+    pyplot.ylabel('Profit in $10,000')
+    pyplot.xlabel('Population of City in 10,000s')
+    pyplot.show()
+
+
+plotData(X, y)
+
+# Add a column of ones to X. The numpy function stack joins arrays along a given axis. 
+# The first axis (axis=0) refers to rows (training examples) 
+# and second axis (axis=1) refers to columns (features).
+X = np.stack([np.ones(m), X], axis=1)
+
+def computeCost(X, y, theta):
+    """
+    Compute cost for linear regression. Computes the cost of using theta as the
+    parameter for linear regression to fit the data points in X and y.
+    
+    Parameters
+    ----------
+    X : array_like
+        The input dataset of shape (m x n+1), where m is the number of examples,
+        and n is the number of features. We assume a vector of one's already 
+        appended to the features so we have n+1 columns.
+    
+    y : array_like
+        The values of the function at each data point. This is a vector of
+        shape (m, ).
+    
+    theta : array_like
+        The parameters for the regression function. This is a vector of 
+        shape (n+1, ).
+    
+    Returns
+    -------
+    J : float
+        The value of the regression cost function.
+    
+    Instructions
+    ------------
+    Compute the cost of a particular choice of theta. 
+    You should set J to the cost.
+    """
+    
+    # initialize some useful values
+    m = y.size  # number of training examples
+    
+    # You need to return the following variables correctly
+    J = 0
+    
+    # ====================== YOUR CODE HERE =====================
+    J = (1 /(2*m)) * (np.sum((np.dot(X, theta) - y) ** 2))
+    
+    # ===========================================================
+    return J
